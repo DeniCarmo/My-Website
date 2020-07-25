e1883c2547aa4e46abd7181c7d4f16319eb7472b
Deni <denilson.gothic@gmail.com>
2020-07-25 17:03:36 -0300

    Floating header, works link button rework, mobile menu corrections, added a maks for mobile menu

--- js/theme.js
+++ js/theme.js
@@ -7,11 +7,12 @@ $(document).ready(() => {
     // Functions
     goto_animation();
     menu_mobile();
+    banner_icon();
+    header();
 
     function goto_animation(){
-        $('a[href^="#"]').on('click', function(e){
+        $body.on('click', 'a[href^="#"]', function(e){
             let target = $(this.getAttribute('href'));
-            console.log(target);
 
             if(target.length){
                 e.preventDefault();
@@ -20,15 +21,41 @@ $(document).ready(() => {
         });
     }
 
+    function banner_icon(){
+        $window.width() >= 1200 ? $('.banner-icon').attr('href', '#header') : $('.banner-icon').attr('href', '#about').addClass('banner-icon--mobile');
+    }
+
     function menu_mobile(){
 
         $body.on('click', '#menuOpen', () => {
             $('#menuMobile').addClass('menu-mobile__toggle');
+            $html.removeClass('mask-active');
+            $html.addClass('mask-active');
         });
 
         $body.on('click', '#menuClose', () => {
             $('#menuMobile').removeClass('menu-mobile__toggle');
+            $html.removeClass('mask-active');
         });
 
+        $body.on('click', '.menu-mobile__item .menu-link', () => {
+            $('#menuClose').trigger('click');
+        });
+
+    }
+
+    function header(){
+
+        $window.scroll(() => {
+
+            if($window.scrollTop() >= $('#main_content').offset().top){
+                $('#header').removeClass('header-sticky');
+                $('#header').addClass('header-sticky');
+                console.log('Header is fixed.');
+            }else{
+                $('#header').removeClass('header-sticky');
+                console.log('Header is relative.');
+            }
+        });
     }
 });
