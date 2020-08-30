3727f619505ce44fb54b888c1b9f4f636d351b19
DeniCarmo <denilson.gothic@gmail.com>
2020-08-29 23:54:46 -0300

    Upgrades, languages, slick carousel, works, responsive

--- js/theme.js
+++ js/theme.js
@@ -11,6 +11,21 @@ $(document).ready(() => {
     header();
     modal_lang();
 
+    $('.works-carousel-js').not('.slick-initialized').slick({
+        arrows: true,
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        lazyLoad: 'ondemand',
+        nextArrow: `<i class="works-carousel-icon works-carousel-icon-next"><svg xmlns="http://www.w3.org/2000/svg" id="Capa_1" enable-background="new 0 0 551.13 551.13" height="512" viewBox="0 0 551.13 551.13" width="512"><path d="m361.679 275.565-223.896 223.897v51.668l275.565-275.565-275.565-275.565v51.668z"/></svg></i>`,
+        prevArrow: `<i class="works-carousel-icon works-carousel-icon-prev"><svg xmlns="http://www.w3.org/2000/svg" id="Capa_1" enable-background="new 0 0 551.13 551.13" height="512" viewBox="0 0 551.13 551.13" width="512"><path d="m189.451 275.565 223.897-223.897v-51.668l-275.565 275.565 275.565 275.565v-51.668z"/></svg></i>`,
+        pauseOnHover: true
+    });
+
     function goto_animation(){
         $body.on('click', 'a[href^="#"]', function(e){
             let target = $(this.getAttribute('href'));
@@ -58,85 +73,68 @@ $(document).ready(() => {
         });
     }
 
-    function setCookie(cname, cvalue, exdays) {
-        var d = new Date();
-        d.setTime(d.getTime() + (exdays*24*60*60*1000));
-        var expires = "expires="+ d.toUTCString();
-        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-    }
-
-    function getCookie(name) {
-        var nameEQ = name + "=";
-        var ca = document.cookie.split(';');
-        for(var i=0; i < ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0)==' ') c = c.substring(1,c.length);
-            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-        }
-        return null;
-    }
-
-    function deleteCookie(name) {   
-        document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-    }
-
     function modal_lang(){
 
-        if(getCookie('modal_lang') == null){
+        $('.lang-switcher-item').click(function(){
 
-            $('.modal-lang').addClass('modal-lang-show');
-            $body.addClass('mask-active');
+            if($('.lang-switcher-item').data('lang') == 'pt'){
+                $('.lang-switcher-item').removeClass('lang-switcher-pt');
+                $('.lang-switcher-item').addClass('lang-switcher-en');
+                $('.lang-switcher-item').text('EN-US');
+                $('.lang-switcher-item').data('lang', 'en');
+            }else{
+                $('.lang-switcher-item').addClass('lang-switcher-pt');
+                $('.lang-switcher-item').removeClass('lang-switcher-en');
+                $('.lang-switcher-item').text('PT-BR');
+                $('.lang-switcher-item').data('lang', 'pt');
+            }
 
-            $('.modal-lang-item__title').click(function(){
-    
-                let lang = $(this).data('lang'),
-                    text_lang = new Lang();
-    
-                if(lang == 'pt'){
-                    text_lang = text_lang.portugueseLang;
-                }else{
-                    text_lang = text_lang.englishLang;
-                }
-    
-                $('.banner-title').text(text_lang.banner_title);
-                $('.banner-subtitle').text(text_lang.banner_text);
-    
-                $('.menu-link-about').text(text_lang.menu_item_about);
-                $('.menu-link-skills').text(text_lang.menu_item_skills);
-                $('.menu-link-tools').text(text_lang.menu_item_tools);
-                $('.menu-link-works').text(text_lang.menu_item_works);
-                $('.menu-link-contact').text(text_lang.menu_item_contact);
-    
-                $('.about-title').text(text_lang.about_title);
-                $('.about-text').text(text_lang.about_text);
-                $('.about-link').text(text_lang.about_link);
-    
-                $('.skills-title').text(text_lang.skills_title);
-                $('.skills-subtitle').text(text_lang.skills_subtitle);
-                $('.skills-item-title').text(text_lang.skills_item1_title);
-                $('.skills-item-text').text(text_lang.skills_item1_subtitle);
-    
-                $('.tools-title').text(text_lang.tools_title);
-    
-                $('.works-title').text(text_lang.jobs_title);
-                $('#works-item1 .works-subtitle').text(text_lang.jobs_item1_subtitle);
-                $('#works-item1 .works-screenshot img').attr('alt', text_lang.jobs_item1_title);
-                $('#works-item1 .works-screenshot img').attr('title', text_lang.jobs_item1_title);
-                $('#works-item1 .works-type').text(text_lang.jobs_item1_type);
-                $('#works-item1 .works-text').text(text_lang.jobs_item1_description);
-                $('#works-item1 .works-link').text(text_lang.jobs_item_link);
-    
-                $('.contact-title').text(text_lang.contact_title);
-    
-                $('.madeby-item__text').text(text_lang.developed_by);
-    
-                $('.modal-lang').removeClass('modal-lang-show');
-                $body.removeClass('mask-active');
-    
-                setCookie('modalLang', lang, 1);
-            });
-        }
+            let lang = $(this).data('lang'),
+                text_lang = new Lang();
 
+            if(lang == 'pt'){
+                text_lang = text_lang.portugueseLang;
+            }else{
+                text_lang = text_lang.englishLang;
+            }
 
+            $('.banner-title').text(text_lang.banner_title);
+            $('.banner-subtitle').text(text_lang.banner_text);
+
+            $('.menu-link-about').text(text_lang.menu_item_about);
+            $('.menu-link-skills').text(text_lang.menu_item_skills);
+            $('.menu-link-tools').text(text_lang.menu_item_tools);
+            $('.menu-link-works').text(text_lang.menu_item_works);
+            $('.menu-link-contact').text(text_lang.menu_item_contact);
+
+            $('.about-title').text(text_lang.about_title);
+            $('.about-text').text(text_lang.about_text);
+            $('.about-link').text(text_lang.about_link);
+
+            $('.skills-title').text(text_lang.skills_title);
+            $('.skills-subtitle').text(text_lang.skills_subtitle);
+            $('.skills-item-title').text(text_lang.skills_item1_title);
+            $('.skills-item-text').text(text_lang.skills_item1_subtitle);
+
+            $('.tools-title').text(text_lang.tools_title);
+
+            $('.works-title').text(text_lang.jobs_title);
+            $('.works-platform').text(text_lang.jobs_platform);
+            $('.works-platform').append(text_lang.jobs_platform_tray_content);
+            $('.works-platform-logo').attr('title', text_lang.jobs_platform_tray_link_title);
+
+            $('#works-item1 .works-subtitle').text(text_lang.jobs_item1_subtitle);
+            $('#works-item1 .works-screenshot img').attr('alt', text_lang.jobs_item1_title);
+            $('#works-item1 .works-screenshot img').attr('title', text_lang.jobs_item1_title);
+            $('#works-item1 .works-text').text(text_lang.jobs_item1_text);
+            $('#works-item1 .works-link').text(text_lang.jobs_button_text);
+            $('#works-item1 .works-link-js').attr('href', text_lang.jobs_item1_link);
+            $('#works-item1 .works-link-js').attr('title', text_lang.jobs_item1_link_title);
+            $('#works-item1 .works-description').text(text_lang.jobs_item1_description);
+
+            $('.contact-title').text(text_lang.contact_title);
+
+            $('.madeby-item__text').text(text_lang.developed_by);
+        });
     }
 });
